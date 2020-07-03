d25540e43e91db498d7b5268e5f89e25724baf39
Newton <kamaunewton78@gmail.com>
2020-07-03 09:54:15 +0300

    wrapped everything inside the runClock function and called it inside the setInterval function to update our hands per second

--- script.js
+++ script.js
@@ -2,17 +2,25 @@ const hourHand = document.querySelector("#hour");
 const minuteHand = document.querySelector("#minute");
 const secondHand = document.querySelector("#second");
 
-//initiate date object and get hours, minutes and seconds
-var date = new Date();
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
 
-// move the hands according to the current time
-let hrPosition = (hr * 360 / 12 ) + (min * (360 /60)/12) ;
-let minPosition = (min * 360 / 60) + (sec * (360/60) / 60 );
-let secPosition = (sec * 360) / 60;
+function runTheClock() {
+    
 
-hourHand.style.transform = "rotate(" + hrPosition + "deg)";
-minuteHand.style.transform = "rotate(" + minPosition + "deg)";
-secondHand.style.transform = "rotate(" + secPosition + "deg)";
+    //initiate date object and get hours, minutes and seconds
+    var date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    // move the hands according to the current time
+    let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
+    let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
+    let secPosition = (sec * 360) / 60;
+
+    hourHand.style.transform = "rotate(" + hrPosition + "deg)";
+    minuteHand.style.transform = "rotate(" + minPosition + "deg)";
+    secondHand.style.transform = "rotate(" + secPosition + "deg)";
+
+}
+
+var interval = setInterval(runTheClock, 1000);
