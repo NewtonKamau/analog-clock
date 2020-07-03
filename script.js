e0c78f40a9471b4cc116c328f412c01b9c74cd57
Newton <kamaunewton78@gmail.com>
2020-07-03 09:47:26 +0300

    the second multiplication on the hour and minute hands is to ensure it moves according to the respective hand ie the minute hand and the second hand respectively

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let min = date.getMinutes();
 let sec = date.getSeconds();
 
 // move the hands according to the current time
-let hrPosition = hr * 360 / 12 ;
+let hrPosition = (hr * 360 / 12 ) + (min * (360 /60)/12) ;
 let minPosition = (min * 360 / 60) + (sec * (360/60) / 60 );
 let secPosition = (sec * 360) / 60;
 
