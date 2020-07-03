e613ec9915f050bd20d54bc375beb0cd0292cf2f
Newton <kamaunewton78@gmail.com>
2020-07-03 09:35:26 +0300

    moved the hour , minutes and seconds hand to specific number and initialized the date object getting hours, minutes, seconds

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const HOURHAND = document.querySelector("#hour");
-const MINUTEHAND = document.querySelector("#minute");
-const SECONDHAND = document.querySelector("#second");
+const hourHand = document.querySelector("#hour");
+const minuteHand = document.querySelector("#minute");
+const secondHand = document.querySelector("#second");
+
+// move the hands to the given number position using js
+let hrPosition = 20;
+let minPosition = 130;
+let secPosition = 267;
+
+//initiate date object and get hours, minutes and seconds 
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+hourHand.style.transform = "rotate(" + hrPosition + "deg)";
+minuteHand.style.transform = "rotate(" + minPosition + "deg)";
+secondHand.style.transform = "rotate(" + secPosition + "deg)";
